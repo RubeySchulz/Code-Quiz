1cb7f241fc16afcd73032d6bbcacaf766debc52e
Ruben <[connor.j.schulz@gmail.com]>
2022-04-08 04:09:24 -0400

    added full website functionality minus a few features

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ var questions = [
         answer1: "numbers and strings",
         answer2: "other arrays",
         answer3: "booleans",
-        rightAnswer: "all of the above"
+        rightAnswer: "all other answers"
     },
     {
         question: "String values must be enclosed within _____ when being assigned to variables.",
@@ -40,7 +40,20 @@ var main = document.querySelector("main");
 var mainInfo = document.querySelector("main > div");
 var headerInfo = document.querySelector("#header")
 var startQuizButton = document.querySelector("#intro-info > button");
+var timer = document.querySelector("#timer");
+var form = document.querySelector("#high-score-form");
+
+//countdown till there are no more questions left
 var questionCounter = 0;
+//what the timer is currently at
+var timerCount = 0;
+//variable for the internal timer
+var countdown;
+//if last answer was wrong or right
+var lastAnswerCorrect;
+//localStorage highscores
+var highScores = [];
+
 
 //Delete everything in main
 var deleteEverything = function(){
@@ -48,7 +61,7 @@ var deleteEverything = function(){
 }
 
 //add question info to the page
-var addQuestion = function(){
+var addQuestion = function(event){
     deleteEverything();
 
     //randomly get a object from the array and take the object off the array
@@ -86,7 +99,6 @@ var addQuestion = function(){
     for(var i = 0; i < 4; i++){
         var wrongAnswers = [currentQuestion.answer1, currentQuestion.answer2, currentQuestion.answer3];
         
-        
         var button = document.createElement("button");
         button.className = "btn answer-btn";
         if(i === random){
@@ -96,28 +108,139 @@ var addQuestion = function(){
         }
         else {
             button.textContent = (i + 1) + ". " + wrongAnswers[counter];
+            button.setAttribute("correct", "no");
             counter++;
         }
         answers.appendChild(button);
     }
 
+    //sets wrong or right footer
+    var lastAnswer = footer();
+
     //add that div to .question-info
-    overall.appendChild(answers);
+    overall.append(answers, lastAnswer);
     main.appendChild(overall);
+
+    //sets main info to to div thats currently loaded into the screen
     mainInfo = document.querySelector("main > div");
 }
 
 var nextQuestion = function(event){
     var targetEl = event.target;
 
+    //sets whether to say wrong or right on the next screen
+    if(targetEl.getAttribute("correct") === "no"){
+        timerCount = Math.max(timerCount -= 15, 0);
+        lastAnswerCorrect = "false";
+    }
+    else if(targetEl.getAttribute("correct") === "yes"){
+        lastAnswerCorrect = "true";
+    }
+    //load another question until you run out of questions, then load the game over screen
     if(questionCounter !== 4 && targetEl.matches(".answer-btn")){
         addQuestion();
         questionCounter++;
+    } 
+    else if(questionCounter === 4 && targetEl.matches(".answer-btn")){
+        allDone();
     }
-    
 }
 
+var startQuiz = function(event){
+    //start first question
+    addQuestion();
 
+    //set timer to 75 seconds
+    timerCount = 75;
+    timer.textContent = "Time: " + timerCount;
+
+    //start the timer to go down once every second until 0
+    countDown = setInterval(timerCountdown, 1000);
+}
+
+var allDone = function(){
+    deleteEverything();
+    //stop timer, update highscore
+    clearInterval(countDown);
+
+    //create overall div
+    var overall = document.createElement("div");
+    overall.className = "end-screen";
+
+    //add title and score count
+    var title = document.createElement("h1");
+    title.textContent = "All Done!";
+    var subtext = document.createElement("p");
+    subtext.textContent = "Your final score is " + timerCount;
+    overall.append(title, subtext);
+
+    //add input section to a form
+    form.innerHTML += "<label for='initials'>Enter Initials: </label><input type='text' placeholder='' name='initials' id='initials'><button class='btn'>Submit</button>";
+
+    //adding wrong or right
+    var lastAnswer = footer();
+    overall.append(form, lastAnswer);
+    main.appendChild(overall);
+
+    //reset mainInfo to current page
+    mainInfo = document.querySelector("main > div");
+}
+
+var enterHighScore = function(event){
+    event.preventDefault();
+    var currentScore = 
+    {
+        name: document.querySelector("input").value,
+        score: timerCount
+    };
+    highScores.push(currentScore);
+
+    deleteEverything();
+
+    
+    var overall = document.createElement("div");
+    overall.className = "scores";
+
+    var title = document.createElement("h1");
+    title.textContent = "High Scores";
+
+    var list = document.createElement("ol");
+    list.id = "scores-list";
+
+
+    overall.append(title, list);
+    overall.innerHTML += "<button class='btn' id='go-back'>Go Back</button><button class='btn' id='clear-scores'>Clear high scores</button>";
+
+    main.appendChild(overall);
+}
+
+var loadScores = function(){
+
+}
+
+var footer = function(){
+    var lastAnswer = document.createElement("h2");
+    lastAnswer.id = "last-answer";
+    if(lastAnswerCorrect === "true"){
+        lastAnswer.textContent = "Correct!";
+    }
+    else if(lastAnswerCorrect === "false"){
+        lastAnswer.textContent = "Wrong!";
+    }
+
+    return lastAnswer;
+}
+
+var timerCountdown = function(){
+    if(timerCount > 0){
+        timerCount--;
+        timer.textContent = "Time: " + timerCount;
+    }
+    else if(questionCounter !== 4) {
+        allDone();
+        clearInterval(countDown);
+    }
+}
 
 //whenever a button is clicked in #question-answers, run the question and answers function again
 //return if the answers was correct or false and deduct time if false, while adding text on bottom saying true or false
@@ -126,6 +249,9 @@ var nextQuestion = function(event){
 
 //after entered, show high score screen with options to clear high scores and go back
 
-startQuizButton.addEventListener("click", addQuestion);
+loadScores();
+startQuizButton.addEventListener("click", startQuiz);
 
 main.addEventListener("click", nextQuestion);
+
+form.addEventListener("submit", enterHighScore);
